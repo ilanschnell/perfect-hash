25eacfd842bbf245d317129f94baae11ba6f7c07
Ilan Schnell <ilanschnell@gmail.com>
2019-05-25 02:40:33 -0500

    cleanup main program

--- perfect_hash.py
+++ perfect_hash.py
@@ -52,15 +52,15 @@ import sys
 import random
 import string
 
-is_py2 = bool(sys.version_info[0] == 2)
+is_py3k = bool(sys.version_info[0] == 3)
 
-if is_py2:
-    from cStringIO import StringIO
-else:
+if is_py3k:
     from io import StringIO
+else:
+    from cStringIO import StringIO
 
 
-verbose = 0
+verbose = False
 trails = 5
 
 
@@ -182,7 +182,7 @@ def generate_hash(kdic, Hash):
     """
     # N is the number of vertices in the graph G
     N = 1 if not kdic else (max(kdic.values()) + 1)
-    if verbose:
+    if verbose >= 2:
         sys.stderr.write('N = %i\n' % N)
 
     trail = 0 # Number of trial graphs so far
@@ -217,7 +217,7 @@ def generate_hash(kdic, Hash):
     if verbose:
         sys.stderr.write('\nAcyclic graph found after %i trails.\n' % trail)
 
-    if verbose:
+    if verbose >= 2:
         sys.stderr.write('N = %i\n' % N)
     if verbose:
         sys.stderr.write('Checking generated hash function... ')
@@ -347,16 +347,16 @@ class Format:
     >>> o.delimiter = ': '
     >>> o.width = 75
     >>> o.indent = 4
-    >>> x = Format(o)
-    >>> x(list(range(10)))
+    >>> x = Format( o )
+    >>> x( range(10) )
     '0: 1: 2: 3: 4: 5: 6: 7: 8: 9'
     >>> o.delimiter = '; '
-    >>> x = Format(o)
-    >>> x(list(range(5)))
+    >>> x = Format( o )
+    >>> x( range(5) )
     '0; 1; 2; 3; 4'
     >>> o.delimiter = ' '
-    >>> x = Format(o)
-    >>> x(list(range(5)), quote = True)
+    >>> x = Format( o )
+    >>> x( range(5), quote = True )
     '"0" "1" "2" "3" "4"'
     >>> x(42)
     '42'
@@ -369,7 +369,7 @@ class Format:
         for name in names:
             setattr(self, name, getattr(options, name))
 
-        if verbose:
+        if verbose >=2:
             sys.stderr.write("Format options:\n")
             for name in names:
                 sys.stderr.write('  %s: %r\n' % (name, getattr(self, name)))
@@ -408,8 +408,8 @@ def keyDict(keys_hashes):
     4
     """
     K = len(keys_hashes)     # number of keys
-    if verbose:
-        sys.stdout.write('K = %i\n' % K)
+    if verbose >= 2:
+        sys.stderr.write('K = %i\n' % K)
 
     kdic = dict(keys_hashes)
     if len(kdic) < K:
@@ -588,10 +588,9 @@ def self_test(options):
                              builtin_template(Hash),
                              Hash,
                              options)
-        if is_py2:
-            exec(code) in {}
+        exec(code) in {}
 
-    verbose = 0
+    verbose = False
     for Hash in [Hash1, Hash2]:
         for K in range(0, 27):
             run(K, Hash)
@@ -615,11 +614,10 @@ def self_test(options):
         if verbose:
             print('Executing code ...')
         flush_dot()
-        if is_py2:
-            exec(code) in {}
+        exec(code) in {}
 
     flush_dot()
-    d = PerfHash(dict([(100 - i, i * i) for i in range(500)]))
+    d = PerfHash(dict([(100-i, i*i) for i in range(500)]))
     for i in range(500):
         assert d[100-i] == i*i
     flush_dot()
@@ -629,8 +627,8 @@ def self_test(options):
     if verbose:
         print('Running doctest ...')
 
-    verbose = 0
-    failure_count, test_count = doctest.testmod(report = True, verbose = 0)
+    verbose = False
+    failure_count, test_count = doctest.testmod(report = True, verbose = False)
     print()
     if failure_count:
         sys.stderr.write('FAILED\n')
@@ -667,7 +665,7 @@ is processed and the output code is written to stdout.
 
     parser.add_option("--indent",
                       action  = "store",
-                      default = 2,
+                      default = 4,
                       type    = "int",
                       help    = "Make INT spaces at the beginning of a "
                                 "new line when generated list is wrapped. "
@@ -772,7 +770,7 @@ is processed and the output code is written to stdout.
     else:
         parser.error("trails before increasing N has to be larger than zero")
 
-    verbose = int(options.verbose or 0)
+    verbose = options.verbose
 
     if options.test:
         self_test(options)
