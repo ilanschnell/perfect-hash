6b3b064d48a406da8872f08bcd7b5e94635a2995
Ilan Schnell <ilanschnell@gmail.com>
2020-06-10 22:35:12 -0500

    improve testing of example

--- examples/PyCExt/test.py
+++ examples/PyCExt/test.py
@@ -4,15 +4,14 @@ from timeit import timeit
 from stations import locator
 
 
-call = sys.argv[1]
-print(repr(call))
-
 D = {}
-N = 1_000_000
-
 for line in open('stations.dat'):
     c, l = [x.strip() for x in line.split(',')]
+    assert locator(c) == l
     D[c] = l
+
+call = 'DL5BAC'
+N = 1_000_000
 
 print(repr(D[call]))
 print(timeit("D[%r]" % call, "from __main__ import D", number=N))
