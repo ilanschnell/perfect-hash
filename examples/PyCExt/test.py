c992bd8bd6d8f49f30e397c86c624a4283951798
Ilan Schnell <ilanschnell@gmail.com>
2020-06-11 01:04:02 -0500

    simplify example code

--- examples/PyCExt/test.py
+++ examples/PyCExt/test.py
@@ -6,15 +6,15 @@ from stations import locator
 
 D = {}
 for line in open('stations.dat'):
-    c, l = [x.strip() for x in line.split(',')]
+    c, l = (x.strip() for x in line.split(','))
     assert locator(c) == l
     D[c] = l
 
 call = 'DL5BAC'
 N = 1_000_000
 
-print(repr(D[call]))
+print(D[call])
 print(timeit("D[%r]" % call, "from __main__ import D", number=N))
 
-print(repr(locator(call)))
+print(locator(call))
 print(timeit("locator(%r)" % call, "from __main__ import locator", number=N))
