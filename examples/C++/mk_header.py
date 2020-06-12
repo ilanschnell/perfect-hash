fc6c057c156a97135da5d163bf07a30005bca7d3
Ilan Schnell <ilanschnell@gmail.com>
2020-06-11 22:48:54 -0500

    remove C++ example

--- examples/C++/mk_header.py
+++
@@ -1,8 +0,0 @@
-for line in open('states.dat'):
-    line = line.strip()
-    if line.startswith('#'):
-        continue
-
-    row = tuple(entry.strip() for entry in line.split('|'))
-
-    print('  {"%s", "%s", %s},' % row)
