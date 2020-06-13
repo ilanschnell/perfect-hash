47c26597d23ae539b2a3a598aea69d965b737799
Ilan Schnell <ilanschnell@gmail.com>
2020-06-13 03:08:09 -0500

    add bash script to run all examples

---
+++ examples/run-all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export PYTHONHASHSEED=0
+for py in *.py
+do
+    python $py || exit 1
+done
+
+for m in */Makefile
+do
+    d=$(dirname $m)
+    pushd $d
+    make test || exit 1
+    make clean
+    popd
+done
+echo "=== all tests OK ==="
