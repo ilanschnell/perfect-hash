ba2f36513a2420b57e7094377c4cfa3c459b9aa8
Ilan Schnell <ilanschnell@gmail.com>
2025-08-31 22:52:25 -0500

    update setup.py

--- examples/PyCExt/setup.py
+++ examples/PyCExt/setup.py
@@ -1,4 +1,7 @@
-from setuptools import setup, Extension
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension
 
 setup(
     ext_modules = [
