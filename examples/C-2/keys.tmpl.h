c08c961069a06b05f26278a55c9e782295513823
Ilan Schnell <ilanschnell@gmail.com>
2025-09-02 02:25:40 -0500

    typo

--- examples/C-2/keys.tmpl.h
+++ examples/C-2/keys.tmpl.h
@@ -1,6 +1,6 @@
 #define NK  $NK       /* number of keys */
 #define NG  $NG       /* number of vertices */
-#define NS  $NS       /* length of array T1 and T2 */
+#define NS  $NS       /* length of array S1 and S2 */
 
 int S1[] = {$S1};
 int S2[] = {$S2};
