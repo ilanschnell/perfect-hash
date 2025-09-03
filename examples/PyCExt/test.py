b88fc6cd4e98a3777a6e6c904d635e91a7cc4c9e
Ilan Schnell <ilanschnell@gmail.com>
2025-09-03 01:15:12 -0500

    improve test

--- examples/PyCExt/test.py
+++ examples/PyCExt/test.py
@@ -1,19 +1,30 @@
-from timeit import timeit
+from time import perf_counter
 
 from stations import locator
 
 
 D = {}
 for line in open('stations.dat'):
-    c, l = (x.strip() for x in line.split(','))
-    assert locator(c) == l
-    D[c] = l
+    call, loc = (x.strip() for x in line.split(','))
+    assert locator(call) == loc
+    D[call] = loc
 
-call = 'DL5BAC'
+call = "DL5BAC"
 N = 1_000_000
 
-print(D[call])
-print(timeit("D[%r]" % call, "from __main__ import D", number=N))
+t0 = perf_counter()
+for _ in range(N):
+    loc1 = D[call]
+t1 = perf_counter()
+for _ in range(N):
+    loc2 = locator(call)
+t2 = perf_counter()
 
-print(locator(call))
-print(timeit("locator(%r)" % call, "from __main__ import locator", number=N))
+print("%s  %8.3f" % (loc1, 1e3 * (t1 - t0)))
+print("%s  %8.3f" % (loc2, 1e3 * (t2 - t1)))
+assert loc1 == loc2 == "JO43LG"
+
+try:
+    locator("W5UN")
+except KeyError:
+    print("OK")
