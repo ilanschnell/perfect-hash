af54c757105a3cae29d1cdcac780ee014a822e90
Ilan Schnell <ilanschnell@gmail.com>
2025-09-04 23:20:51 -0500

    simplify example C-2

--- examples/C-2/main.c
+++ examples/C-2/main.c
@@ -9,17 +9,15 @@
 /* return index of key in K if key is found, -1 otherwise */
 int get_index(const char *key)
 {
-    int i, f1 = 0, f2 = 0;
+    int f1 = 0, f2 = 0, i;
+    unsigned char c;
 
-    for (i = 0; key[i] && i < NS; i++) {
-        unsigned char c = key[i];
+    for (i = 0; (c = key[i]) && i < NS; i++) {
         f1 += S1[i] * c;
         f2 += S2[i] * c;
-        f1 %= NG;
-        f2 %= NG;
     }
-    i = (G[f1] + G[f2]) % NG;
-    if (i < NK && strcmp(key, K[i]) == 0)
+    i = (G[f1 % NG] + G[f2 % NG]) % NG;
+    if (strcmp(key, K[i]) == 0)
         return i;
 
     return -1;
@@ -29,12 +27,9 @@ int main()
 {
     int i;
 
-    char *junk = "acnhuvn5yushvghnw7og5siuhgsiuhnglsh45vgghwn";
-
-    assert(get_index(junk) == -1);
+    assert(get_index("A.B") == -1);
 
     for (i = 0; i < NK; i++) {
-        /* printf("i=%d    %s     %d\n", i, K[i], get_index(K[i])); */
         assert(get_index(K[i]) == i);
     }
 
